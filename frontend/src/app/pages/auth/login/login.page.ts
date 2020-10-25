ee121504134e74250fb49c7f4122f7cbe260cf45
Mantis <mantis.nk@gmail.com>
2020-10-25 03:59:59 +0300

    LOGIN PAGE KAI AUTH STO BACK

--- frontend/src/app/pages/auth/login/login.page.ts
+++ frontend/src/app/pages/auth/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+//import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  //  private auth: AuthService
+  ) { }
 
+  // TODO: HANDLE HARDWARE BACK BUTTON
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required,Validators.minLength(6)]),
+      autologin: new FormControl(false, [Validators.required])
+    });
+  }
+
+  login() {
+    const { email, password } = this.loginForm.value
+
+    this.router.navigateByUrl('')
+  //  this.auth.login(email, password)
+
   }
 
 }
