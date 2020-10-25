3da74ae7cb4cb910254e000790e3573f5cb7841c
Nikos Mantis <mantis.nk@gmail.com>
2020-10-25 15:31:48 +0200

    profile html/style

--- frontend/src/app/pages/tab3/tab3.page.ts
+++ frontend/src/app/pages/tab3/tab3.page.ts
@@ -7,6 +7,54 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  public profile_segment:string;
+
+  // You can get this data from your API. This is a dumb data for being an example.
+  public images = [
+    {
+      id: 1,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/e35/13473123_1544898359150795_654626889_n.jpg'
+    },
+    {
+      id: 2,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/e35/12940826_1673029922963013_921771297_n.jpg'
+    },
+    {
+      id: 3,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/e15/10852865_738738146215825_1258215298_n.jpg'
+    },
+    {
+      id: 4,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/e15/891528_841068522581102_1591061904_n.jpg'
+    },
+    {
+      id: 5,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-frx5-1.cdninstagram.com/t51.2885-15/e35/10809765_1474804169496730_887570428_n.jpg'
+    },
+    {
+      id: 6,
+      username: 'candelibas',
+      profile_img: 'https://avatars1.githubusercontent.com/u/918975?v=3&s=120',
+      post_img: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/e15/891515_1524153351163603_439436363_n.jpg'
+    }
+  ];
+
+  constructor() {  
+  }
+
+  // Define segment for everytime when profile page is active
+  ionViewWillEnter() {
+    this.profile_segment = 'grid';
+  }
 
 }
